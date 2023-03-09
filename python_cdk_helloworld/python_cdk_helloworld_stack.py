f9874585c5157498866bd134ac2ae21b1ceb1821
Brian Rook <brook@happymoney.com>
2023-03-08 19:11:17 -0700

    feat: use version in cdk deploy

--- python_cdk_helloworld/python_cdk_helloworld_stack.py
+++ python_cdk_helloworld/python_cdk_helloworld_stack.py
@@ -13,9 +13,13 @@ class PythonCdkHelloworldStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.build_lambda_func()
+        self.build_lambda_func(kwargs.get("image_tag"))
 
-    def build_lambda_func(self):
+    def build_lambda_func(self, image_tag):
+        self.ecr_image = _lambda.DockerImageCode.from_ecr(
+            repository=Repository.from_repository_name(self, "helloWorld-cdk-repository", "python-cdk-helloworld"),
+            tag=image_tag
+        )
         self.prediction_lambda = _lambda.DockerImageFunction(
             scope=self,
             id="helloWorld-cdk",
@@ -23,9 +27,7 @@ class PythonCdkHelloworldStack(Stack):
             function_name="helloWorld-cdk",
             # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
             # a docker image on deployment
-            code=_lambda.DockerImageCode.from_ecr(
-                repository=Repository.from_repository_name(self, "helloWorld-cdk-repository", "python-cdk-helloworld")
-            ),
+            code=self.ecr_image,
         )
 
         api = apigateway.RestApi(self, "helloworld-cdk-api",
