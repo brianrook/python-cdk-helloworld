0e1b6e8929022a6d737dca4c658959c761d74fa2
Brian Rook <brook@happymoney.com>
2023-03-09 17:29:54 -0700

    fix: hc api key

--- python_cdk_helloworld/HelloWorldLambda/lambda.py
+++ python_cdk_helloworld/HelloWorldLambda/lambda.py
@@ -4,11 +4,13 @@ import logging.config
 logging.config.fileConfig('logging.conf')
 
 from helloworld.helloworld import HelloWorld
+from datadog_lambda.wrapper import datadog_lambda_wrapper
 
 
 logger = logging.getLogger(__name__)
 
 
+@datadog_lambda_wrapper
 def handler(event, context):
     logger.info("handler request in")
     return {
