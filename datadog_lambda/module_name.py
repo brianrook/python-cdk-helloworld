889958e820bd668962f22fef5488097ae2cd6edf
Brian Rook <brook@happymoney.com>
2023-03-09 17:44:45 -0700

    Revert "fix: hc api key"
    This reverts commit 0e1b6e8929022a6d737dca4c658959c761d74fa2.

--- datadog_lambda/module_name.py
+++
@@ -1,3 +0,0 @@
-def modify_module_name(module_name):
-    """Returns a valid modified module to get imported"""
-    return ".".join(module_name.split("/"))
--- datadog_lambda/stats_writer.py
+++
@@ -1,9 +0,0 @@
-class StatsWriter:
-    def distribution(self, metric_name, value, tags=[], timestamp=None):
-        raise NotImplementedError()
-
-    def flush(self):
-        raise NotImplementedError()
-
-    def stop(self):
-        raise NotImplementedError()
