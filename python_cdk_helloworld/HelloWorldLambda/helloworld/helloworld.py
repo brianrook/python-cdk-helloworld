b22da69586834ccf2a54f1114d98234017ebfdd3
Brian Rook <brook@happymoney.com>
2023-03-10 07:51:15 -0700

    fix: message for demo

--- python_cdk_helloworld/HelloWorldLambda/helloworld/helloworld.py
+++ python_cdk_helloworld/HelloWorldLambda/helloworld/helloworld.py
@@ -15,6 +15,6 @@ class HelloWorld:
     @staticmethod
     def getMessage():
         logger.info("request to helloworld.getmessage")
-        msgResponse = MessageResponse(message="Hello World! with version")
+        msgResponse = MessageResponse(message="Hello World! Demo Day")
         logger.debug("returning msg: %s", msgResponse.json())
         return msgResponse
